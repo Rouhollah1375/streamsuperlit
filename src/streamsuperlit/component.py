4a8f3af2036425d99e344c60bf9110197070a159
Rouhollah Abolhasani <roohi.abol@gmail.com>
2024-12-11 14:25:41 +0330

    The functionality of Page and Component is now unified inside the Component class, essentially obscuring the Page class entirely. Now, a Component can serve as a page (within pages/ directory) or can be nested inside other Components. As a side effect, this unification also obscures the responsibility of SSTCore. Therefore, this class is also removed from the codebase, making the framework more lightweight and simpler.

--- src/streamsuperlit/component.py
+++ src/streamsuperlit/component.py
@@ -1,6 +1,7 @@
 from streamsuperlit.view import View
 from streamsuperlit.controller import Controller
-from streamsuperlit.core.lifecycle_hook import OnInit, AfterInit
+from streamsuperlit.lifecycle_hook import OnInit, AfterInit
+
 import streamlit as st
 
 class Component:
@@ -15,18 +16,18 @@ class Component:
                     controller_cls: Controller.__class__) -> None:
         self._name = name
         self._id = id
-        self._view: View = view_cls(f'{id}-view')
-        self._controller: Controller = controller_cls(f'{id}-controller')
 
-        self._view.register_controller(self._controller)
-        self._controller.register_view(self._view)
+        if not hasattr(self, '_view_ctrl_initialized'):
+            self._view: View = view_cls(f'{id}-view')
+            self._controller: Controller = controller_cls(f'{id}-controller')
+            self._view.register_controller(self._controller)
+            self._controller.register_view(self._view)
+            self._view_ctrl_initialized = True
 
-        OnInit.handle(self._controller)
-        OnInit.handle(self._view)
+            OnInit.handle(self._controller)
+            OnInit.handle(self._view)
+            AfterInit.handle(self._controller)
+            AfterInit.handle(self._view)
 
-        AfterInit.handle(self._controller)
-        AfterInit.handle(self._view)
-
-
-    def get_view(self):
-        return self._view
+    def __call__(self) -> None:
+          self._view.render()
